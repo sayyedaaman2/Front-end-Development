a0df93bcb6ec6eb3e2869008beabaa4e98b0fcd2
Sayyed Aaman <sayyedaaman9@gmail.com>
2022-11-21 21:43:08 +0530

    update css

--- crm/client/src/App.js
+++ crm/client/src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import React  from "react";
+
 
 import Navbar from "./Components/Navbar"
 import SignUp from './Components/SignUp';
@@ -8,7 +10,6 @@ function App() {
   return (
     <>
       <Navbar/>
-      {/* <SignUp/> */} 
       <SignUpEng/>
     </>
   );
