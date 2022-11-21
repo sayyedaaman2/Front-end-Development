02d09fac895b9f5efa8236db6b263b4b85918486
Sayyed Aaman <sayyedaaman9@gmail.com>
2022-11-22 01:39:25 +0530

    update the the css

--- crm/client/src/Components/Login.js
+++ crm/client/src/Components/Login.js
@@ -1,7 +1,43 @@
 import loginPic from '../img/login-icon.png';
 
 
+import React, {useState} from "react";
+
 function Login() {
+
+    const [userId, setUserId] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [userIdErr, setUserIdErr] = useState(false);
+    const [passwordErr, setPasswordErr] = useState(false);
+
+    //userId handler
+    function userIdHandler(e){
+        let userId = e.target.value;
+        console.log("userId" , userId)
+
+        var validUserIdRegex = new RegExp(/^[a-zA-Z][^\s-]+$/);
+        if (validUserIdRegex.test(userId)) {
+            setUserIdErr(false);
+        } else {
+            setUserIdErr(true);
+        }
+        setUserId(userId);
+    }
+
+    function passwordHandler(e) {
+        let password = e.target.value;
+        console.log("password" , password)
+        var validPasswordRegex = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/);
+        //S.Aaman$1234
+        if (validPasswordRegex.test(password)) {
+            setPasswordErr(false);
+        } else {
+            setPasswordErr(true);
+        }
+        setPassword(password);
+    }
+
     return (
         <>
             <div id='login-container'>
@@ -17,8 +53,8 @@ function Login() {
                                         UserId
                                     </td>
                                     <td className='input-field'>
-                                        <input type="text" name='userId' placeholder='Enter your UserId' autoComplete='off' />
-                                        {/* <div className="errorIcon">{name != "" ? nameErr ? <span className="falseMark">&#215;</span> : <span className="trueMark">&#10003;</span> : <span> </span>}</div> */}
+                                        <input type="text" name='userId' placeholder='Enter your UserId' autoComplete='off' onChange={userIdHandler} />
+                                        <div className="errorIcon">{userId != "" ? userIdErr ? <span className="falseMark">&#215;</span> : <span className="trueMark">&#10003;</span> : <span> </span>}</div>
                                     </td>
                                 </tr>
                                 <tr className='row'>
@@ -26,8 +62,8 @@ function Login() {
                                         Password
                                     </td>
                                     <td className='input-field'>
-                                        <input type="text" name='password' placeholder='Enter your Password' autoComplete='off' />
-                                        {/* <div className="errorIcon">{name != "" ? nameErr ? <span className="falseMark">&#215;</span> : <span className="trueMark">&#10003;</span> : <span> </span>}</div> */}
+                                        <input type="text" name='password' placeholder='Enter your Password' autoComplete='off' onChange={passwordHandler}/>
+                                        <div className="errorIcon">{password != "" ? passwordErr ? <span className="falseMark">&#215;</span> : <span className="trueMark">&#10003;</span> : <span> </span>}</div>
                                     </td>
                                 </tr>
                             </tbody>
